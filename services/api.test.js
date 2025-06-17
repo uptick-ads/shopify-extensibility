ee99d81717847e37195642d0061c1848065d4e51
aaronbhansen <aaronbhansen@gmail.com>
2025-06-17 11:06:24 -0600

    Updating tests for new flow url

--- services/api.test.js
+++ services/api.test.js
@@ -68,7 +68,7 @@ function createApi(includeShopApi = true, { integrationId = null } = {}) {
 
 function generateFlowURL(api, placement, integrationID = null) {
   if (isPresent(integrationID)) {
-    expect(api.flowURL).toBe(`https://api.uptick.com/v2/places/${integrationID}/flows/new`);
+    expect(api.flowURL).toBe(`https://api.uptick.com/places/${integrationID}/flows/new`);
   } else {
     expect(api.flowURL).toBe("https://api.uptick.com/places/flows/shopify");
   }
@@ -139,19 +139,19 @@ describe("api", () => {
 
       // These are all the same for test or development
       expect(api.baseURL).toBe("https://api.uptick.com");
-      expect(api.flowURL).toBe("https://api.uptick.com/v2/places/AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE/flows/new");
+      expect(api.flowURL).toBe("https://api.uptick.com/places/AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE/flows/new");
       expect(api.captureException).toBeDefined();
       expect(api.captureWarning).toBeDefined();
       expect(api.setLoading).toBeDefined();
       expect(api.flow).toBeNull();
     });
 
-    test("sets defaults with integrationId for v1 on constructor", async () => {
+    test("apiVersion is ignored on constructor with new flow url with integrationId", async () => {
       let api = new Api({ integrationId: "AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE", apiVersion: "v1" });
 
       // These are all the same for test or development
       expect(api.baseURL).toBe("https://api.uptick.com");
-      expect(api.flowURL).toBe("https://api.uptick.com/v1/places/AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE/flows/new");
+      expect(api.flowURL).toBe("https://api.uptick.com/places/AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE/flows/new");
       expect(api.captureException).toBeDefined();
       expect(api.captureWarning).toBeDefined();
       expect(api.setLoading).toBeDefined();
